be1eb22a10557dec37443584fab88322824b2b7a
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-03-30 17:16:31 +0530

    worked on front-end

---
+++ client/src/components/ChannelSearch.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { useChatContext } from 'stream-chat-react';
+import { SearchIcon } from '../assets';
+
+const ChannelSearch = () => {
+
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onSearch = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setQuery(e.target.value);
+  }
+
+  return (
+    <div className='channel-search__container'>
+      <div className='channel-search__input__wrapper'>
+        <div className='channel-search__input__icon'>
+          <SearchIcon/>
+        </div>
+        <input 
+          type="text"
+          className='channel-search__input__text' 
+          placeholder='Search'
+          value={query}
+          onChange={onSearch}
+        />
+      </div>      
+    </div>
+  )
+}
+
+export default ChannelSearch
